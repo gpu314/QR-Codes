1bb24c675da2c4a83683608312842b155eb638cb
Grace <33011813+gpu314@users.noreply.github.com>
2024-12-23 23:38:17 -0500

    functions for matrix data bits

--- matrix_generation.py
+++ matrix_generation.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 # Determines size (side length) of QR code from version
 def size(version: int) -> int:
@@ -132,6 +132,63 @@ def preamble(matrix: List[List[int]], version: int, size: int) -> List[List[int]
     return matrix
 
 
+# Next data bit to place data at
+def next_data_bit(matrix: List[List[int]], size: int, xydv: Tuple[int]) -> Tuple[int]:
+    # Convert coordinates for easier math handling
+    x = size-1-xydv[0]
+    y = size-1-xydv[1]
+    d = xydv[2] # direction: 1 = up, -1 = down
+    v = xydv[3] # reached vertical bar: 0 = no, 1 = yes
+
+    # Pair pixels pattern
+    if (x + v) % 2 == 0:
+        x += 1
+    # Vertical pattern
+    else:
+        x -= 1
+        if d == 1:
+            y += 1
+        else:
+            y -= 1
+    
+    # Wrap around top/bottom
+    if y < 0:
+        y += 1
+        x += 2
+        d = 1
+    elif y >= size:
+        y -= 1
+        x += 2
+        d = -1
+    
+    # Skip over vertical timing pattern
+    if x == size-FINDER_OUTER:
+        x += 1
+        v = 1
+    
+    # Convert coordinates back to actual values
+    x = size-1-x
+    y = size-1-y
+
+    return (x, y, d, v)
+
+
+# Place data bits
+def data_bits(matrix: List[List[int]], size: int, dataBits: str) -> List[List[int]]:
+    idx = 0
+    curr = (size-1, size-1, 1, 0)
+    x = curr[0]
+    y = curr[1]
+    while curr[0] > -2:
+        x = curr[0]
+        y = curr[1]
+        # Only place data bit if pixel at coordinates not already occupied from preamble
+        if x >= 0 and y >= 0 and x < size and y < size and matrix[y][x] == -1:
+            matrix[y][x] = dataBits[idx]
+            idx += 1
+        curr = next_data_bit(matrix, size, curr)
+    return matrix
+
 # Testing, display matrix, temporary function
 def print_matrix(matrix: List[List[int]]):
     for row in matrix:
@@ -146,8 +203,10 @@ def print_matrix(matrix: List[List[int]]):
 
 # Testing
 if __name__ == "__main__":
-    version = 8
+    version = 5
     size = size(version)
     mat = empty_matrix(size)
     mat = preamble(mat, version, size)
+    data = "01000011111101101011011001000110010101011111011011100110111101110100011001000010111101110111011010000110000001110111011101010110010101110111011000110010110000100010011010000110000001110000011001010101111100100111011010010111110000100000011110000110001100100111011100100110010101110001000000110010010101100010011011101100000001100001011001010010000100010001001011000110000001101110110000000110110001111000011000010001011001111001001010010111111011000010011000000110001100100001000100000111111011001101010101010111100101001110101111000111110011000111010010011111000010110110000010110001000001010010110100111100110101001010110101110011110010100100110000011000111101111011011010000101100100111111000101111100010010110011101111011111100111011111001000100001111001011100100011101110011010101111100010000110010011000010100010011010000110111100001111111111011101011000000111100110101011001001101011010001101111010101001001101111000100010000101000000010010101101010001101101100100000111010000110100011111100000010000001101111011110001100000010110010001001111000010110001101111011000000000"
+    mat = data_bits(mat, size, data)
     print_matrix(mat)
